3cb7ffb60ae3a426cbfe2cfe3816d8979cbd56a6
fuku <naa01044@nifty.com>
2025-04-15 16:26:35 +0900

    Revert "3"
    This reverts commit 5057897f53ca67daf077cb4d5dcf2f72bf6e5464.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  'vim-jp/vimdoc-ja',
-  'thinca/vim-zenspace',
-  { 'delphinus/emcl.nvim', opts = {} },
-  { 'windwp/nvim-ts-autotag', opts = {} },
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons', opts = {} },
-}
+return {}
